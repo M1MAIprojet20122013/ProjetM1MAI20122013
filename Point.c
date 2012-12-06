60b66e8eceb4b99879b1c76ac15c89d0a958f420
hoffmana <alexandre.hoffmann.etu@gmail.com>
2012-12-06 16:18:33 +0100

    Point

---
+++ Point.c
@@ -0,0 +1,53 @@
+#include "Point.h"
+
+Point new_point(double x, double y, double z)
+{
+	Point p = {.x = x, .y = y, .z = z};
+	return p;
+}
+
+double scalaire(const Vector* v1, const Vector* v2)
+{
+	return v1->x*v2->x + v1->y*v2->y + v1->z*v2->z;
+}
+
+double norm(const Vector* v)
+{
+	return sqrt(square(v->x) + square(v->y));
+}
+
+void set_point(Point* p, double x, double y, double z)
+{
+	p->x = x;
+	p->y = y;
+	p->z = z;
+}
+
+double d(Point p1, Point p2)
+{
+	return sqrt(square(p1.x - p2.x) + square(p1.y - p2.y) + square(p1.z - p2.z));
+}
+
+Point Point_add(const Point* p1, const Point* p2)
+{
+	Point p3 = {.x=p1->x + p2->x, .y=p1->y + p2->y, .z=p1->z + p2->z};
+	return p3;
+}
+
+Point Point_sub(const Point* p1, const Point* p2)
+{
+	Point p3 = {.x=p1->x - p2->x, .y=p1->y - p2->y, .z=p1->z - p2->z};
+	return p3;
+}
+
+Point Point_mul(const Point* p1, const Point* p2)
+{
+	Point p3 = {.x=p1->x * p2->x, .y=p1->y * p2->y, .z=p1->z * p2->z};
+	return p3;
+}
+
+Point Point_div(const Point* p1, const Point* p2)
+{
+	Point p3 = {.x=p1->x / p2->x, .y=p1->y / p2->y, .z=p1->z / p2->z};
+	return p3;
+}
---
+++ Point.h
@@ -0,0 +1,41 @@
+#ifndef POINT_H
+#define POINT_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+// STL includes
+#include <math.h>
+// other includes
+#include "commun.h"
+
+typedef struct s_Point Point;
+typedef Point Vector;
+
+struct s_Point
+{
+	double x;
+	double y;
+	double z;
+};
+
+Point new_point(double x, double y, double z);
+
+void set_point(Point* p, double x, double y, double z);
+double d(Point p1, Point p2);
+
+double scalaire(const Vector* v1, const Vector* v2);
+double norm(const Vector* v);
+
+Point Point_add(const Point* p1, const Point* p2);
+Point Point_sub(const Point* p1, const Point* p2);
+Point Point_mul(const Point* p1, const Point* p2);
+Point Point_div(const Point* p1, const Point* p2);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
