95440f8d6554c6c4e93fe87ae40fc4598a24f4e8
hoffmana <alexandre.hoffmann.etu@gmail.com>
2012-12-06 16:17:56 +0100

    plot

---
+++ plot.c
@@ -0,0 +1,63 @@
+#include "plot.h"
+
+static FILE* h_gnuplot;
+static uint plot_id = 0;
+
+void init_gnuplot()
+{
+	printf("Initializing gnuplot...\n");
+	h_gnuplot = popen("gnuplot --persist", "w");
+	if (h_gnuplot == NULL)
+	{
+		printf("Initializing gnuplot [FAILED]\n");
+	}
+	else
+	{
+		printf("Initializing gnuplot [  OK  ]\n");
+	}
+}
+
+void plot_from(const char* filename, const char* options)
+{
+	if (plot_id == 0)
+	{
+		fprintf(h_gnuplot, "plot '");
+	}
+	else
+	{
+		fprintf(h_gnuplot, "replot '");
+	}
+	fprintf(h_gnuplot, filename);
+	fprintf(h_gnuplot, "' ");
+	fprintf(h_gnuplot, options);
+	fprintf(h_gnuplot, "\n\n");
+	fflush(h_gnuplot);
+}
+
+void plot(const Point* points, uint length, const char* options)
+{
+	char filename[15];
+	sprintf(filename, "/tmp/plot-%i", plot_id);
+	FILE* f = fopen(filename, "w");
+	if (f == NULL)
+	{
+		fprintf(stderr, "[ ERROR ] cannot save dataset to a temp file");
+	}
+	else
+	{
+		uint i;
+		for (i=0;i<length;++i)
+		{
+			fprintf(f, "%f %f %f\n", points[i].x, points[i].y, points[i].z);
+		}
+		fclose(f);
+		plot_from(filename, options);
+		++plot_id;
+	}
+}
+
+
+int close_gnuplot()
+{
+	return pclose(h_gnuplot);
+}
---
+++ plot.h
@@ -0,0 +1,22 @@
+#ifndef PLOT_H
+#define PLOT_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+// STL includes
+#include <stdio.h>
+// other includes
+#include "Point.h"
+
+void init_gnuplot();
+void plot_from(const char* filename, const char* options);
+void plot(const Point* points, uint length, const char* options);
+int close_gnuplot();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
